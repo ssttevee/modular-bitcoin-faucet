d00051ce822f14ff85dd08f525d11a86c588fbc6
Steve Lam <stevyo99@yahoo.ca>
2015-05-12 11:32:49 -0700

    cloudflare is making this difficult...

--- lib/AdManager.php
+++ lib/AdManager.php
@@ -8,7 +8,7 @@ class AdManager {
     );
 
     static function insertGoogleAd($pubid, $slot, $ignoreblacklist = false) {
-        if(!$ignoreblacklist && in_array($_SERVER['REMOTE_ADDR'], self::$ip_blacklist)) return;
+        if(!$ignoreblacklist && (in_array($_SERVER['REMOTE_ADDR'], self::$ip_blacklist) || in_array($_SERVER['HTTP_CF_CONNECTING_IP'], self::$ip_blacklist))) return;
         print <<<EOF
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <ins class="adsbygoogle" style="display:inline-block;width:336px;height:280px" data-ad-client="$pubid" data-ad-slot="$slot"></ins>
@@ -17,14 +17,14 @@ EOF;
     }
 
     static function insertAdbitAd($slot, $ignoreblacklist = false) {
-        if(!$ignoreblacklist && in_array($_SERVER['REMOTE_ADDR'], self::$ip_blacklist)) return;
+        if(!$ignoreblacklist && (in_array($_SERVER['REMOTE_ADDR'], self::$ip_blacklist) || in_array($_SERVER['HTTP_CF_CONNECTING_IP'], self::$ip_blacklist))) return;
         print <<<EOF
 <iframe scrolling="no" frameborder="0" src="//adbit.co/adspace.php?a=$slot" style="overflow:hidden;width:468px;height:60px;"></iframe>
 EOF;
     }
 
     static function insertBitClixAd($slot, $ignoreblacklist = false) {
-        if(!$ignoreblacklist && in_array($_SERVER['REMOTE_ADDR'], self::$ip_blacklist)) return;
+        if(!$ignoreblacklist && (in_array($_SERVER['REMOTE_ADDR'], self::$ip_blacklist) || in_array($_SERVER['HTTP_CF_CONNECTING_IP'], self::$ip_blacklist))) return;
         print <<<EOF
         <iframe scrolling="no" style="border: 0; width: 728px; height: 90px;" src="//ads.bcsyndication.com/get.php?s=$slot"></iframe>
 EOF;
