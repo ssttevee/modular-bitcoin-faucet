b2b4d74a425091f08c6dad845fe1a5ec25cb66d8
Steve Lam <stevyo99@yahoo.ca>
2015-05-12 01:08:08 -0700

    What's wrong now T.T

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-//error_reporting(0);
-//ini_set('display_errors', 0);
+error_reporting(0);
+ini_set('display_errors', 0);
 
 if(isset($_GET['ref'])) setcookie('ref', $_GET['ref'], time() + 3600, '/');
 
