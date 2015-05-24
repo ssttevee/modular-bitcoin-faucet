4b7225c83151b850b0c5b7cc60750ab04a9b1b9d
Steve Lam <stevyo99@yahoo.ca>
2015-05-24 03:00:32 -0700

    clean and new directive

---
+++ js/claim.js
@@ -0,0 +1,12 @@
+btcFaucetApp.directive('claimForm', [function() {
+
+    var link = function(scope, element, attrs) {
+
+        ACPuzzle.create('GFWOoXyYExNZgFBgTogSqX3Xgr.qUPWE', 'acwidget');
+    };
+
+    return {
+        restrict: 'A',
+        link: link
+    };
+}]);
