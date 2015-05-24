c17aa2d42a2bd58f64106b1966f5c846bf7226a3
Steve Lam <stevyo99@yahoo.ca>
2015-05-24 04:03:11 -0700

    this should work...

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+ob_start();
 require_once __DIR__ . "/vendor/autoload.php";
 
 use \AllTheSatoshi\FaucetManager;
--- js/claim.js
+++ js/claim.js
@@ -1,6 +1,21 @@
-btcFaucetApp.directive('claimForm', [function() {
+btcFaucetApp.directive('claimForm', ['$notice', function($notice) {
 
     var link = function(scope, element, attrs) {
+
+        scope.claim = function() {
+            var data = element.serializeArray();
+            $.ajax({
+                method: "POST",
+                url: "./ajax/" + attrs.game + "/claim.json",
+                data: {captcha_challenge: data.adcopy_challenge, captcha_response: data.adcopy_response}
+            }).done(function(msg) {
+                var json = JSON.parse(msg);
+                $notice.getEventForm({
+                    event: json.success ? "success" : "error",
+                    message: json.message
+                }).attr("action", "./" + attrs.game + ".html").submit();
+            });
+        }
 
     };
 
