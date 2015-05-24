0f09811d379422bf7b01ec102083be2b74f6de11
Steve Lam <stevyo99@yahoo.ca>
2015-05-24 03:24:19 -0700

    can't load captcha asynchronously :(

--- js/claim.js
+++ js/claim.js
@@ -2,7 +2,6 @@ btcFaucetApp.directive('claimForm', [function() {
 
     var link = function(scope, element, attrs) {
 
-        ACPuzzle.create('GFWOoXyYExNZgFBgTogSqX3Xgr.qUPWE', 'acwidget');
     };
 
     return {
