f7e5cdc97cc1fa9d736aca282688a15e815e56ee
Steve Lam <stevyo99@yahoo.ca>
2015-05-29 18:55:29 -0700

    brought back composer T.T

--- autoload.php
+++ autoload.php
@@ -1,8 +1,5 @@
 <?php
 
-require_once("modules/modloader.php");
-require_once("src/AdManager.php");
-spl_autoload_register(function($class) {
-    $namespace = explode("\\", $class);
-    require_once(__DIR__ . "/src/" . $namespace[count($namespace) - 1] . ".php");
-});
+require_once(__DIR__ . "/modules/modloader.php");
+require_once(__DIR__ . "/src/AdManager.php");
+require_once(__DIR__ . "/vendor/autoload.php");
